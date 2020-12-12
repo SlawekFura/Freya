a45a1a12fb9d106e7e790c03bae22f55f4320a42
SlawekFura <slawek.fura@gmail.com>
2020-12-12 01:52:45 +0100

    Continue...

---
+++ GcodeGenerator/2D/ConfigGenerator.py
@@ -0,0 +1,55 @@
+import dxfgrabber as dg
+import sys
+
+ParameterList = ["thickness", "toolDiameter", "bothSideMilled", "shouldGenSerialGcode"]
+
+
+class ConfigGenerator:
+
+    def __init__(self):
+        pass
+
+    def generate(self, baseString):
+        return self.extractConfig(baseString)
+
+    def extractConfig(self, stringToProcess):
+        lines = stringToProcess.split("^J")
+        newLines = []
+        for line in lines:
+            line = line.replace(' ', '')
+            line = line.replace('\n', '')
+            if line != "":
+                newLines.append(line)
+
+        layerToconfigMap = {}
+
+        for line in newLines:
+            layerToconfigMap.update(self.extractConfigElement(line))
+
+        for key, value in layerToconfigMap.items():
+            print(key, value)
+
+        return layerToconfigMap
+
+    def extractConfigElement(self, stringLine):
+        [leftSide, rightSide] = stringLine.split("=")
+
+        splitParameters = leftSide.replace(')', '').split("(")
+        layerName = splitParameters[0]
+        params = splitParameters[1].split(",")
+
+        splitValues = rightSide.split(",")
+
+        configMap = {layerName: {}}
+        valueSideIdx = 0
+        for param in params:
+            if layerName == "MATERIAL":
+                configMap[layerName] = splitValues[0]
+                break
+            if param not in ParameterList:
+                sys.exit("Unsupported param name: " + param + " for layer:" + layerName)
+
+            configMap[layerName].update({param: splitValues[valueSideIdx]})
+            valueSideIdx += 1
+
+        return configMap
